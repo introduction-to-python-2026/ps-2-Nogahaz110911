1ef088a854ed133d95dead93501ad582a6e317ea
Nogahaz110911 <nogahaziza@gmail.com>
2025-11-11 23:14:28 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,15 +1,20 @@
 def find_max_number(num1, num2, num3):
     if num1 >= num2 and num1 >= num3:
       return num1
-    if num2>=num3 and num2 >= num3:
+    if num2 >= num3 and num2 >= num3:
       return num2
     return num3
+
 def find_mean(num1, num2, num3):
-    total = num1 + num2 + num3
-    mean_value = total / 3
-    return mean_value
+    n = 3
+    sum_of_numbers = num1 + num2 + num3
+    mean = sum_of_numbers / n
+    return mean
+    
 
 def find_mean_std(num1, num2, num3):
+    n = 3
     mean = find_mean(num1, num2, num3)
-    pass  # Replace 'pass' with code
-
+    sum_of_squares = (num1 - mean) **2 + (num2 - mean) **2 + (num3 - mean) **2
+    std = (sum_of_squares / n) **0.5
+    return mean, std
